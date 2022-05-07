58d111dcfa930550a317c2100ae2119e9bb519df
Zatloukal, Jan <zatloukal.jan@gmail.com>
2022-05-07 09:40:40 +0200

    Add main file

---
+++ __main__.py
@@ -0,0 +1,15 @@
+from sneepy.config import Config
+from sneepy.generator import Generator
+from sneepy.api import Api
+import webview
+
+generator = Generator()
+config = Config()
+html = generator.generate()
+
+api = Api()
+window_size = config.getWindowSize()
+print(window_size)
+window = webview.create_window(title='Sneepy', html=html, js_api=api, width=window_size[0], height=window_size[1])
+api.setWindow(window)
+webview.start(debug=True)
